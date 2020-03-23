c83cc0a1c96b057a44f37e385e9d4947c38fed2e
guanarp <60052556+guanarp@users.noreply.github.com>
2020-03-22 23:30:58 -0300

    Update prueba2.py
    arreglado el merge

--- prueba2.py
+++ prueba2.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
+
 import RPi.GPIO as GPIO
-=======
-import RPI.GPIO as GPIO
->>>>>>> 0aacc0904b3d39094f04d4deba37aa8c5a4d2aad
 from time import sleep 
 
 GPIO.setmode(GPIO.BOARD)
@@ -12,11 +9,7 @@ rojo = 12
 naranja = 37
 verde = 36
 amarillo = 38
-<<<<<<< HEAD
 azul = 33
-=======
-azul = 35
->>>>>>> 0aacc0904b3d39094f04d4deba37aa8c5a4d2aad
 
 class Luces:
     def __init__(self, port, status = 0 ):
@@ -27,10 +20,6 @@ class Luces:
     def changeStatus(self):
         self.status = (self.status +1)%2
         GPIO.output(self.port,self.status)
-<<<<<<< HEAD
-=======
-        sleep(0.5)
->>>>>>> 0aacc0904b3d39094f04d4deba37aa8c5a4d2aad
 
 lista_luces=[]
 lista_luces.append( Luces(rojo) )
@@ -39,7 +28,6 @@ lista_luces.append( Luces(verde) )
 lista_luces.append( Luces(amarillo) )
 lista_luces.append( Luces(azul) )
 
-<<<<<<< HEAD
 try:
 	while True:
 		for i in lista_luces:
@@ -48,11 +36,3 @@ try:
 except KeyboardInterrupt:
 	GPIO.cleanup()
 	
-=======
-while True:
-    try:
-        for i in lista_luces:
-            lista_luces[i].changeStatus()
-
-
->>>>>>> 0aacc0904b3d39094f04d4deba37aa8c5a4d2aad
